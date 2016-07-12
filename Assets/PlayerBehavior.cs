059bdfa4cf0ba0092a8784a3f988634fb98ced06
Asaf Gartner <asaf.gart@gmail.com>
2016-07-12 15:00:50 +0300

    poop

---
+++ Assets/PlayerBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerBehavior : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var cam = GetComponent<Camera>();
+        cam.enabled = isLocalPlayer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

4064e44c87b1bed537f38b5884b7708c923a0f43
Shay Davidson <sdavidson@paypal.com>
2016-07-12 22:40:16 +0300

    test

---
+++ Assets/Float.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour {
+	public float amplitude;          //Set in Inspector 
+	public float speed;                  //Set in Inspector 
+	private float tempVal;
+	private Vector3 tempPos;
+	void Start () 
+	{
+		tempVal = transform.position.y;
+	}
+	void Update ()
+	{   
+		tempPos.x = transform.position.x;
+		tempPos.z = transform.position.z;
+		tempPos.y = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+		transform.position = tempPos;
+	}
+}

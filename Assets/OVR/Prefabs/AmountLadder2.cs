072c119b7e64dfa6c932f2f19b863d160230da07
Asaf Gartner <asaf.gart@gmail.com>
2016-07-12 22:37:35 +0300

    Fixed ladder

--- Assets/OVR/Prefabs/AmountLadder2.cs
+++ Assets/OVR/Prefabs/AmountLadder2.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class AmountLadder2 : MonoBehaviour {
 
-	public Transform spine;
-	public Transform slide;
+	public GameObject spine;
+	public GameObject slide;
 
 //	private boolean spawned;
 
@@ -12,6 +12,8 @@ public class AmountLadder2 : MonoBehaviour {
 	private float DISTANCE_M = 4f;
 	private float ANGLE_RAD = 3f * (Mathf.PI / 180f);
 	private int NUM_SPINES = 20;
+
+    private ArrayList existingLadder;
 
 	// Use this for initialization
 	void Start () {
@@ -23,32 +25,52 @@ public class AmountLadder2 : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.B)) {
 			SpawnLadder ();
 		}
+
+        if (existingLadder != null)
+        {
+            // TODO: raycast
+        }
 	}
 
-	// TODO animate
 	void SpawnLadder() {
-		float firstAngle = -(1.5f + 8f*3f) * (Mathf.PI / 180f);
+        if (existingLadder != null)
+        {
+            removeLadder();
+        }
+        existingLadder = new ArrayList();
+		float firstAngle = -(1.5f + 4f*3f) * (Mathf.PI / 180f);
 
 		for (int i = 0; i < NUM_SPINES; i++) {
 			Vector3 forward = transform.forward;
 			forward.y = 0;
 			forward.Normalize ();
 
+            forward = Quaternion.Euler(0, 20, 0) * forward;
+
 			forward *= Mathf.Cos (firstAngle + i * ANGLE_RAD);
 			forward.y = Mathf.Sin (firstAngle + i * ANGLE_RAD);
 			forward.Normalize ();
 
 			Vector3 position = forward * DISTANCE_M + transform.position;
-			Instantiate (spine, position, Quaternion.LookRotation(position - transform.position));
+            Object spinePart = Instantiate (spine, position, Quaternion.LookRotation(position - transform.position) * Quaternion.Euler(90, 0, 0));
+            existingLadder.Add(spinePart);
 
 			if (i == 0) {
-				Instantiate(slide, position, Quaternion.LookRotation(position - transform.position));
+                Object slidePart = Instantiate(slide, position, Quaternion.LookRotation(position - transform.position) * Quaternion.Euler(90, 0, 0));
+                existingLadder.Add(slidePart);
 			}
 		}
 	}
 
 	void removeLadder() {
-		// TODO
+        if (existingLadder != null)
+        {
+            foreach(Object t in existingLadder)
+            {
+                Destroy(t);
+            }
+            existingLadder = null;
+        }
 	}
 
 }

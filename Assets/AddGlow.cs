e9082aa232ecd49c7ffa1c45a2835eb000d90393
Shay Davidson <sdavidson@paypal.com>
2016-07-13 01:34:47 +0300

    Shaders

--- Assets/AddGlow.cs
+++ Assets/AddGlow.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class AddGlow : MonoBehaviour {
 
+	public GameObject mesher;
 	private Renderer renderer;
 	private Shader shader1;
 	private Shader shader2;
 
 	// Use this for initialization
 	void Start () {
-		renderer = GetComponent<Renderer>();
+		renderer = mesher.GetComponent<Renderer>();
 		shader1 = Shader.Find("Standard");
 		shader2 = Shader.Find("MK/MKGlow/Normal/DiffuseRim");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (true) {
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Transform cam = Camera.main.transform;
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Raycast (cam.position, cam.forward, out hit, 30) && hit.transform.name.StartsWith ("artik")) {
 			renderer.sharedMaterial.shader = shader2;
 		} else {
 			renderer.sharedMaterial.shader = shader1;
